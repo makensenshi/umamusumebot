0a788cefcfaebd483194c22aba9031c80a86b0a9
scottle <79183256+makensenshi@users.noreply.github.com>
2021-11-19 22:36:14 +0900

    umamusume
    昨日触れたばかりで何もわかりません

---
+++ umabot.py
@@ -0,0 +1,25 @@
+from typing import Text
+import discord
+from discord.ext import commands
+import asyncio
+import glob
+
+
+
+client = discord.Client()
+
+bot = commands.Bot(command_prefix="!")
+bot.remove_command("help")
+
+@bot.event
+async def on_ready():
+    activity = discord.Game(name="ウマ娘エロ画像厳選中…", type=3)
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+    print("起動完了")
+    
+
+
+ 
+    
+
+bot.run("OTEwNzY5NTU2NDAwNzk5NzU0.YZXqPw.XX5Oim8mz9-PFLf85S3x2eG39Ms")
